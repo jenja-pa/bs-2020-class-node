1339eeb3d81e1565d3f1d7a60935d79f300fb663
Evgen Panasko <jenja.pa@gmail.com>
2020-05-12 22:49:53 +0300

    First prepare commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/* index.js */
+
+// импорт модуля "http"
+const http = require('http');
+const port = 3000;
+
+// коллбек на каждый HTTP-запрос
+const requestHandler = (request, response) => {
+    console.log(request.url)
+    console.log(request)
+    response.end('Ping-Pong')
+};
+
+// создание HTTP-сервера
+const server = http.createServer(requestHandler);
+
+// начало прослушивания HTTP-сервера
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('Ошибочка вышла', err)
+    }
+    console.log(`Сервер запущен по адресу http://localhost:${port}`)
+});
