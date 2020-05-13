7c71b239bb7a77fd9dcf05d0ff6064db42e50263
jenja.pa <jenja.pa@gmail.com>
2020-05-13 17:30:19 +0300

    Add body-parser pack & add handler for use in us express server . All work beautiful

--- express.js
+++ express.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-app.get("/:route", (req, res) =>{
-  console.log(`Обробка GET: ${req.url}`);
-  console.log("query:", req.query.a);
-  console.log("params:", req.params.route);
-  res.send("Hello World");
-});
-app.get("/test", (req, res) =>{
-  console.log(`Обробка GET/test: ${req.url}`);
-  res.send("HW test");
-});
+//дані форм
+app.use(bodyParser.urlencoded({extended: false}));
+// файл json
+app.use(bodyParser.json());
 
-app.put("*", (req, res) =>{
-  console.log(`Обробка PUT: ${req.url}`);
-  res.send("Put Hello World");
+// app.get("/:route", (req, res) =>{
+//   console.log(`Обробка GET: ${req.url}`);
+//   console.log("query:", req.query.a);
+//   console.log("params:", req.params.route);
+//   res.send("Hello World");
+// });
+// app.get("/test", (req, res) =>{
+//   console.log(`Обробка GET/test: ${req.url}`);
+//   res.send("HW test");
+// });
+
+app.post("/user", (req, res) =>{
+  console.log(`запит POST: ${req.url}`);
+  console.log("req.body:", req.body);
+  res.send("POST - Hello World");
 });
 
 app.listen(3000);
