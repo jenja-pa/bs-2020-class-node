4605911aa9d1a397b20e76a540b9751f789c2913
jenja.pa <jenja.pa@gmail.com>
2020-05-13 16:24:44 +0300

    Add some addres for processing

--- express.js
+++ express.js
@@ -2,8 +2,17 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) =>{
-  console.log(`Обробка: ${req.url}`);
+  console.log(`Обробка GET: ${req.url}`);
   res.send("Hello World");
+});
+app.get("/test", (req, res) =>{
+  console.log(`Обробка GET/test: ${req.url}`);
+  res.send("HW test");
+});
+
+app.put("*", (req, res) =>{
+  console.log(`Обробка PUT: ${req.url}`);
+  res.send("Put Hello World");
 });
 
 app.listen(3000);
